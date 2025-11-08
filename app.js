acacbb82ea3bf89f1a74d4ad8d42bc397b007e24
Efraim Munthe <efrayanglain@gmail.com>
2025-11-08 20:25:36 +0700

    serve html file, add paths

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-import fs from "node:fs"
+import fs from "node:fs";
+import { createServer } from "node:http";
 
-const files = fs.readdirSync(".")
+const path = "../";
+const files = fs.readdirSync(path);
+const filteredFiles = files.filter((f) => f.match(/.*\.jpe?g$/));
 
-for (const file of files) {
-  if (file.startsWith("simurgh")) {
-  console.log(file)
+const server = createServer((req, res) => {
+  console.log(req.method, req.url);
+
+  if (req.url.endsWith("/")) {
+    const pathToIndex = "./static/index.html";
+
+    // https://dev.to/webduvet/static-content-server-with-nodejs-without-frameworks-d61
+    fs.readFile(pathToIndex, (err, data) => {
+      if (err) console.error(err);
+
+      return res.end(data);
+    });
   }
-}
+
+  if (req.url == "/files") {
+    res.writeHead(200, { "content-type": "application/json" });
+    return res.end(JSON.stringify({ filtered_files: filteredFiles }));
+  }
+});
+
+const PORT = "3000";
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
