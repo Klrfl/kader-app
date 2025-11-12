7a774421f09d93e51666143854fe61d40c7ada16
Efraim Munthe <efrayanglain@gmail.com>
2025-11-12 07:11:04 +0700

    feat: add edit student, display address

--- src/actions/students.ts
+++ src/actions/students.ts
@@ -7,6 +7,10 @@ export const studentUpdate = defineAction({
   input: z.object({
     id: z.number().int().positive(),
     name: z.string(),
+    nickname: z.string().optional(),
+    blood_type: z.string().optional(),
+    instagram_handle: z.string().optional(),
+    date_of_birth: z.coerce.date().optional(),
     group_id: z.number().int().positive(),
     has_bonded_with: z.boolean(),
   }),
@@ -16,6 +20,8 @@ export const studentUpdate = defineAction({
       .set({
         id: input.id,
         name: input.name,
+        nickname: input.nickname,
+        blood_type: input.blood_type,
         group_id: input.group_id,
         has_bonded_with: Number(input.has_bonded_with),
       })
