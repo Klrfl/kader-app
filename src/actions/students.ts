f77bd1c9751523e73cecb41136d63cab3b3fa98c
Efraim Munthe <efrayanglain@gmail.com>
2025-11-18 20:38:35 +0700

    feat: update student image, display input errors

--- src/actions/students.ts
+++ src/actions/students.ts
@@ -2,6 +2,9 @@ import { db } from "@/database";
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { sql } from "kysely";
+import fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 export const studentUpdate = defineAction({
   accept: "form",
@@ -14,6 +17,7 @@ export const studentUpdate = defineAction({
     date_of_birth: z.coerce.date().optional(),
     group_id: z.number().int().positive(),
     has_bonded_with: z.boolean(),
+    image: z.instanceof(File).optional(),
   }),
   handler: async (input) => {
     // TODO: handle errors
@@ -21,20 +25,53 @@ export const studentUpdate = defineAction({
       ? new Date(input.date_of_birth).toISOString()
       : null;
 
-    const result = await db
-      .updateTable("students")
-      .set({
-        id: input.id,
-        name: input.name,
-        nickname: input.nickname,
-        blood_type: input.blood_type,
-        group_id: input.group_id,
-        date_of_birth: sql`datetime(${parsed_dob})`,
-        has_bonded_with: Number(input.has_bonded_with),
-      })
-      .where("id", "=", input.id)
-      .returningAll()
-      .execute();
+    const result = await db.transaction().execute(async (tx) => {
+      const [student] = await tx
+        .updateTable("students")
+        .set({
+          id: input.id,
+          name: input.name,
+          nickname: input.nickname,
+          blood_type: input.blood_type,
+          group_id: input.group_id,
+          date_of_birth: sql`datetime(${parsed_dob})`,
+          has_bonded_with: Number(input.has_bonded_with),
+        })
+        .where("id", "=", input.id)
+        .returningAll()
+        .execute();
+
+      if (input.image) {
+        const group = await tx
+          .selectFrom("groups")
+          .select(["name"])
+          .where("id", "=", input.group_id)
+          .executeTakeFirstOrThrow();
+
+        const wd = path.dirname(fileURLToPath(import.meta.url));
+        const imagesBase = path.join(wd, "../../public/images/");
+
+        const buf = Buffer.from(await input.image.arrayBuffer());
+
+        const trimmedNim = student.nim!.slice(student.nim!.length - 3);
+        const [_, ext] = input.image.type.split("/");
+        const filename = `${group.name}.${trimmedNim}-${input.nickname}.${ext}`;
+
+        const absFilename = path.join(imagesBase, filename);
+
+        await fs.writeFile(absFilename, buf);
+
+        console.log("successfully written file to ", absFilename);
+
+        const imageResult = await tx
+          .updateTable("images")
+          .set({ filename: filename, student_id: student.id })
+          .where("student_id", "=", student.id)
+          .execute();
+      }
+
+      return student;
+    });
 
     return result;
   },
