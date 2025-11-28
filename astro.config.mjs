5d654e30fa0f01f80a260a17ac423778ce2ddaec
Efraim Munthe <efrayanglain@gmail.com>
2025-11-28 22:42:36 +0700

    fix month regression thing
    date.getMonth() returns the index not the month. add one to fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwind()],
+    server: {
+      allowedHosts: ["kader.munthe.dev"],
+    },
   },
+
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
+
