2b486514b9d5302ac18229154641fb9765736b94
Efraim Munthe <efrayanglain@gmail.com>
2025-12-04 09:15:22 +0700

    actually use env variables

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,8 +2,16 @@ import SQLite from "better-sqlite3";
 import { Kysely, SqliteDialect } from "kysely";
 import type { DB } from "./database.types";
 
+if (!import.meta.env.DATABASE_URL) {
+  console.warn(
+    "WARNING: DATABASE_URL not set in .env file. defaulting to default database path (database.sqlite)"
+  );
+}
+
+const DATABASE_URL = import.meta.env.DATABASE_URL || "./database.sqlite";
+
 const dialect = new SqliteDialect({
-  database: new SQLite("./database.sqlite"),
+  database: new SQLite(DATABASE_URL),
 });
 
 export const db = new Kysely<DB>({
