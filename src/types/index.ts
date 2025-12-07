b538a2e1ed91b9467a48ff40e8e5e50d6e23816e
Efraim Munthe <efrayanglain@gmail.com>
2025-12-07 12:27:08 +0700

    implement new feature to create new group

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import type { Images, Students } from "@/database/database.types";
+import type { Images, Students, Groups } from "@/database/database.types";
 import type { Updateable } from "kysely";
 import type { Insertable } from "kysely";
 import type { Selectable } from "kysely";
@@ -14,3 +14,5 @@ export interface Group {
 export type Student = Selectable<Students>;
 export type InsertableStudent = Insertable<Students>;
 export type UpdateableStudent = Updateable<Students>;
+
+export type InsertableGroup = Insertable<Groups>;
